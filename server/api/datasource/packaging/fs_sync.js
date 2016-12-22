804cccf046e077d4a3f893e5254e2e5e1b0585b9
Gene <gene@noonian.org>
2016-12-22 15:47:25 -0800

    Major refactor of packaging functionality; allows for - filesystem-syncing w/ meaningful filenames, - much cleaner json package files/processing - history tracking in UpdateLog records

---
+++ server/api/datasource/packaging/fs_sync.js
@@ -0,0 +1,315 @@
+/*
+Copyright (C) 2016  Eugene Lockett  gene@noonian.org
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * datasource/packaging/fs_sync
+ * @module db._svc.PackagingService
+ * 
+ * Contains logic for syncing data with filesystem for "collaborative" packages. 
+ * 
+ */
+
+var Q = require('q');
+var _ = require('lodash');
+var fs = require('fs');
+var stringify = require('json-stable-stringify');
+ 
+
+var serverConf = require('../../../conf');
+
+var PkgService = require('./index');
+var FieldTypeService = require('../fieldtypes.js');
+
+
+
+
+//Filename format for fs-persisted business objects:
+//<obj display str>.[<fieldName>].<extension>
+var fileRegex = /^(?:.+)\.\[([^\.]+)?\]\.(.{22})\.(\w+)$/; 
+
+
+//Helper for simple directory create
+var mkdir = function(dir) {
+	try {fs.mkdirSync(dir);} catch(err) {}
+};
+
+
+// Helper to generate a sanitized filename for a business object
+var badCharsRegex = /\/|\||\\/g;
+var generateFileName = function(bo) {
+    var str = bo._disp;
+    if(!str) {
+        return bo._id;
+    }
+    return str.replace(badCharsRegex, '_');
+};
+
+
+////////////////////////////////////////////////////////////////////////
+// SYNC OPERATIONS             /////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+
+//Lil function to write a value to a stream, unmodified.
+var identityFn = function(stream, value) { return stream.write(''+value); };
+
+/**
+ * Export (and explode) a single business object to the file system
+ */
+var writeObjectToPackageDir = 
+exports.writeObjectToPackageDir = function(fsPath, bo) {
+	var className = bo._bo_meta_data.class_name;
+	var classDir = fsPath+'/'+className;
+	mkdir(classDir);
+	
+	var targetPath = classDir+'/'+generateFileName(bo);
+	
+	var tdMap = bo._bo_meta_data.type_desc_map;
+	
+	var toBaseFile = {_id:bo._id, __ver:bo.__ver};
+	
+	//First, find the fields that should be put in separate files
+	_.forEach(tdMap, function(td, fieldName) {
+		if(fieldName === '_disp') return;
+		
+		var fth = FieldTypeService.getFieldTypeHandler(td);
+		if(fth && fth.toFileSystem && bo[fieldName]) {
+			var spec = fth.toFileSystem(td);
+			var filePath = targetPath+'.['+fieldName+'].'+bo._id+'.'+spec.extension;
+			console.log('...writing %s', filePath);
+			var ws = fs.createWriteStream(filePath);
+			var writeFn = spec.writeFn || identityFn;
+			writeFn(ws, bo[fieldName]);
+			ws.end();
+		}
+		else {
+			toBaseFile[fieldName] = bo[fieldName];
+		}
+	});
+	
+	//Finish up by writing the base file
+	var filePath = targetPath+'.[].'+bo._id+'.json';
+	console.log('...writing %s', filePath);
+	var ws = fs.createWriteStream(filePath);
+	ws.end(stringify(toBaseFile, {space:'\t'}));
+};
+
+
+/**
+ * Remove an object from filesystem
+ */
+exports.removeObjectFromPackageDir = function(fsPath, bo) {
+	try {
+		var className = bo._bo_meta_data.class_name;
+		var classDir = fsPath+'/'+className;
+		var objFiles = fs.readdirSync(classDir);
+		_.forEach(objFiles, function(fileName) {
+			if(fileName.indexOf('].'+bo._id+'.') > 0) {
+				console.log('deleting %s/%s', classDir, fileName);
+				fs.unlink(classDir+'/'+fileName);
+			}
+		});
+	}
+	catch(err) {
+		console.error('problem deleting object %s from package %s', bo._id, pkgKey);
+		console.error(err);
+	}
+};
+
+
+
+////////////////////////////////////////////////////////////////////////
+// IMPORTING             ///////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * Load a filesystem-exploded package into the system.
+ * Called on every start-up when packageDev is configured for a given package
+ * @param srcDir directory containing pkg_meta.json and a directory for each class
+ * 
+ * @return promise fulfilled when import of srcDir is complete
+ */
+exports.packageFromFs = function(srcDir) {
+    console.log('Loading package from %s...', srcDir);
+    
+    //sync pkg_meta.json (export of a BusinessObjectPackage object) in base of pkg directory
+	var bopFile = srcDir+'/pkg_meta.json';
+    var bopObj = JSON.parse(fs.readFileSync(bopFile));
+    
+    
+    return PkgService.importObject('BusinessObjectPackage', bopObj).then(function() {
+        
+        var promiseChain = Q(true);
+        
+		var classDirs = fs.readdirSync(srcDir);
+		
+        //Bump up BusinessObjectDef to be processed first:
+		var bodIndex = classDirs.indexOf('BusinessObjectDef');
+		if(bodIndex > -1) {
+			classDirs[bodIndex] = classDirs[0];
+			classDirs[0] = 'BusinessObjectDef';
+		}
+		
+		_.forEach(classDirs, function(className) {
+			promiseChain = promiseChain.then(importClassDir.bind(null, srcDir, className));
+		});
+		
+		return promiseChain;
+	});
+	
+	
+};
+
+
+/**
+ * @private
+ * Imports objects from the specified source directory.
+ */
+function importClassDir(srcDir, className) {
+	var promiseChain = Q(true);
+    		
+	var objDir = srcDir+'/'+className;
+	var dirStat = fs.statSync(objDir);
+	if(dirStat.isDirectory()) {
+		console.log(' Loading objects from %s', className);
+		
+		//Need to reconstruct the object potentially from multiple files.
+		//...construct index of id->fileinfo
+		var nameMap = {};
+		var objFiles = fs.readdirSync(objDir);
+		_.forEach(objFiles, function(fileName) {
+			var filePath = objDir+'/'+fileName;
+			var match = fileRegex.exec(fileName); 
+			if(match && match.length >= 4) {
+                ///^(?:.+)\.\[([^\.]+)?\]\.(.{22})\.(\w+)$/
+				var fieldName = match[1];
+                var objId = match[2];
+				var ext = match[3];
+				if(!nameMap[objId]) {
+					nameMap[objId] = {};
+				}
+				if(fieldName) {
+					nameMap[objId][fieldName] = filePath;
+				}
+				else {
+					nameMap[objId]._ = filePath;
+				}
+			}
+		});
+        
+		var jsonObjects = [];
+		
+		//Now use the info from idMap to reconstruct the objects
+		_.forEach(nameMap, function(fileMap) {
+            try {
+                var obj = JSON.parse(fs.readFileSync(fileMap._));
+                _.forEach(fileMap, function(fileName, field) {
+                    if(field !== '_') {
+                        obj[field] = fs.readFileSync(fileName, 'utf8');
+                    }
+                });
+                jsonObjects.push(obj);
+            }
+            catch(e) {
+                console.error('ERROR LOADING %s OBJECT FROM FILE %s', className, fileMap._);
+                console.error(e);
+            }
+		});
+        
+        //If we're dealing with BusinessObjectDef's, make sure abstract super-classes go first
+        if(className === 'BusinessObjectDef') {
+            jsonObjects.sort(function(x,y) {
+              if(!y.abstract === !x.abstract) return 0;
+              else if(y.abstract && !x.abstract) return 1;
+              else return -1;
+            });
+        }
+        
+        //Import the list
+        _.forEach(jsonObjects, function(obj) {
+            promiseChain = promiseChain.then(PkgService.importObject.bind(null, className, obj));
+        });
+        
+	}
+	return promiseChain;
+};
+
+
+
+////////////////////////////////////////////////////////////////////////
+// EXPORTING ///////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+var writeMetaFile = 
+exports.writeMetaFile = function(targetDir, bop) {
+    fs.createWriteStream(targetDir+'/'+'pkg_meta.json')
+        .end(stringify(bop, {space:'\t'}));
+};
+
+/**
+  * Explode a BOP to the filesystem -
+  * A utility to take a distributable, non-collaborative package and make it collaborative.
+  * 
+  * Exports objects in the manifest, and places BusinessObjectPackage 
+  * export itself in the base as pkg_meta.json
+  * 
+  * Places files into server/data_pkg/<key> directory, which can subesquently be moved 
+  * to a separate directory and its own git repo.
+  * 
+ */
+exports.packageObjectsToFs = function(bop) {
+  var targetDir = PKG_DIR+'/'+bop.key;
+  mkdir(targetDir);
+  console.log('Writing package to %s', targetDir);
+  
+  var promiseArr = [];
+  
+  if(bop.manifest) {
+    //First, dump the bop itself to the pkg_meta.json in the base
+    writeMetaFile(targetDir, bop);
+  
+    //Dump the objects listed manifest to filesystem    
+    _.forEach(bop.manifest, function(idVerMap, className){
+      //Place data into data_pkg/<package key>/<bo class>/
+      var objDir = targetDir+'/'+className;
+      mkdir(objDir);
+      
+      //for each object in the manifest...
+      _.forEach(idVerMap, function(ver, objId) {
+        
+        promiseArr.push(
+          db[className].findOne({_id:objId}).then(function(obj) {
+              //Temporary force version id:
+              //obj.__ver = ver;
+            writeObjectToPackageDir(bop.key, obj);
+          })
+        );
+
+      });
+    });
+  }
+  else {
+	console.error('attempted to write pkg that doesnt have a manifest');
+  }
+  
+
+  return Q.all(promiseArr);
+};
+
+
+
+
---
+++ server/api/datasource/packaging/pkg_stream.js
@@ -0,0 +1,380 @@
+/*
+Copyright (C) 2016  Eugene Lockett  gene@noonian.org
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * datasource/packaging/pkg_stream
+ * @module db._svc.PackagingService
+ * 
+ * Contains logic for processing noonian package "streams" - packages 
+ * contained w/in a single .json file, as array of "updates"
+ * 
+ */
+
+var Q = require('q');
+var _ = require('lodash');
+
+var oboe = require('oboe'); //library to allow reading stream of large JSON object without holding all in memory at once
+var jsonStringify = require('json-stable-stringify'); //library to allow stringified JSON to have consistent ordering of keys
+
+var db = require('../index');
+var PkgService = require('./index');
+var GridFsService = require('../gridfs');
+
+
+var stringify = function(obj) {
+    return jsonStringify(obj, {space:'\t'});
+}
+
+
+var PkgVersion = function(major, minor) {
+    this.major = major;
+    this.minor = minor;
+    this.compareTo = function(otherVer) {
+        if(this.major < otherVer.major ||
+          (this.major === otherVer.major && this.minor < otherVer.minor)
+        )
+        return -1;
+        
+        if(this.major > otherVer.major ||
+          (this.major === otherVer.major && this.minor > otherVer.minor)
+        )
+        return 1;
+      return 0;
+    }
+    this.toString = function() {
+      return (this.major || '0')+'.'+(this.minor || '0');
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////
+// INSTALL PACKAGE       ///////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+/**
+ * 
+ */
+exports.installPackage = function(pkgReadStream) {
+    
+  var deferred = Q.defer();
+
+  var bop;
+
+  var promiseChain;
+
+
+  oboe(pkgReadStream)
+  .node('metadata', function(metaObj) {
+      
+    //See if we have an existing version of this package installed
+    if(db.BusinessObjectPackage) {
+      promiseChain = db.BusinessObjectPackage.findOne({_id:metaObj._id});
+    }
+    else {
+      //if we're in DB bootstrap, we don't have the BusinessObjectPackage to work with...
+      //  pass a dummy object as BOP to the next step
+      promiseChain = Q({bootstrap:true});
+    }
+
+    promiseChain = promiseChain.then(function(resultBop) {
+        
+      bop = resultBop;
+      // console.log(bop);
+
+      var installedVer = new PkgVersion(bop.major_version, bop.minor_version);
+      var targetVer = new PkgVersion(metaObj.major_version, metaObj.minor_version);
+
+      if(bop && !bop.bootstrap) {
+        //We're performing an upgrade to an existing package
+        
+        
+        if( installedVer.compareTo(targetVer) >= 0) {
+          // installed version is equal or newer
+          throw 'version incompatibility; cannot go from '+installedVer.toString()+' to '+targetVer.toString();
+        }
+      }
+      else if(!bop) {
+        bop = new db.BusinessObjectPackage();
+      }
+
+      //TODO dependency check: metaObj.dependencies against system
+
+      //update the bop
+      _.assign(bop, metaObj);
+    });
+
+  })
+
+  .node('!.business_objects.*', function(obj) {
+    promiseChain = promiseChain.then(
+        PkgService.importObject.bind(null, obj._class, obj)
+    );
+
+    return oboe.drop;  //As we process the list, don't retain data in memory
+  })
+
+  .fail(function(err) {
+    deferred.reject(err);
+  })
+
+  .done(function() {
+    promiseChain.then(function() {
+      if(!bop.bootstrap) {
+        return bop.save();
+      }
+      else if(db.BusinessObjectPackage) {
+          //if we were bootstrapping, the BusinessObjectPackasge should now be available
+        bop = new db.BusinessObjectPackage(bop);
+        return bop.save();
+      }
+
+    }).then(function() {
+      deferred.resolve(bop);
+    }, function(err) {
+      deferred.reject(err);
+    });
+  });
+
+  return deferred.promise;
+
+}
+
+
+
+////////////////////////////////////////////////////////////////////////
+// BUILD PACKAGE            ////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+
+/**
+ *  Run against a BusinessObjectPackage (BOP) record;
+ *  - builds the package file, incorporating all UpdateLog's associated w/ the BOP
+ *  - stores it in gridfs, sets as package_file attachment to BOP
+ *  - updates manifest and increments minor version on BOP record
+**/
+exports.buildPackage = function(bopId) {
+  
+  var deferred = Q.defer();
+  
+  var bop;              //The BusinessObjectPackage object
+  var mergedManifest;   //The updated manifest (that incorporates UpdateLog's)
+  var pkgStream;        //The stream to which the package json is written.
+  
+  var abstractBods = {}; //tells us which BOD's in the packages are marked 'abstract' (need to output first)
+
+  //Phase 1: 
+  //  grab the BusinessObjectPackage we're working with, and query for all of the UpdateLogs
+  //  and use to pull together mergedManifest
+  Q.all([
+    db.BusinessObjectPackage.findOne({_id:bopId}),
+    db.UpdateLog.find({package:bopId}).sort({timestamp:-1})
+  ])
+  .then(function(resultArr) {
+      
+    bop = resultArr[0];
+    var updateLogs = resultArr[1];
+    
+    mergedManifest = bop.manifest ? _.clone(bop.manifest) : {};
+    
+    
+    //Merge in UpdateLog records
+    var manifestUpdates = {};
+    _.forEach(updateLogs, function(ul) {
+        var forClass = manifestUpdates[ul.object_class];
+        if(!forClass) {
+            forClass = manifestUpdates[ul.object_class] = {};
+        }
+        if(!forClass[ul.object_id]) {
+            if(ul.update_type !== 'delete') {
+                forClass[ul.object_id] = ul.updated_version;
+            }
+            else {
+                forClass[ul.object_id] = 'deleted';
+            }
+        }
+    });
+    
+    _.forEach(Object.keys(manifestUpdates), function(className) {
+        if(!mergedManifest[className]) {
+            mergedManifest[className] = {};
+        }
+        _.assign(mergedManifest[className], manifestUpdates[className]);
+    });
+    
+    
+    //Do a pass through the manifest to preprocess and ensure all version id's are up-to-date
+    //  (because we're streaming the meta object first, which includes the manifest)
+    var checkBo = function(className, bodCheck, boId, boStub) {
+        //console.log('CHECKING %s.%s -> %j', className, boId, boStub);
+        if(!boStub) {
+            console.error('BusinessObject %s.%s missing from DB but no "delete" UpdateLog was found', className, boId);
+            mergedManifest[className][boId] = 'deleted'; //Presume deleted
+            return;
+        }
+        
+        mergedManifest[className][boId] = boStub.__ver;
+        if(bodCheck && boStub.abstract) {
+            abstractBods[boId] = true;
+        }
+    };
+    
+    var versionCheckPromises = [];
+    _.forEach(mergedManifest, function(idVerMap, className) {
+        var projection = {__ver:1};
+        var bodCheck = false;
+        if(className === 'BusinessObjectDef') {
+            projection.abstract = 1;
+            bodCheck = true;
+        }
+        _.forEach(idVerMap, function(v, id) {
+            if(v !== 'deleted') {
+                versionCheckPromises.push(
+                    db[className].findOne({_id:id}, projection).then(checkBo.bind(null, className, bodCheck, id))
+                );
+            }
+        });
+    }); //end forEach(mergedManifest)
+    
+    return Q.all(versionCheckPromises);
+    
+    Q.all(versionCheckPromises).then(); //end Q.all(versionCheckPromises)
+    
+  }) //end initial BOP and UpdateLog lookup
+  .then(function() {
+    //Phase 2:
+    //  mergedManifest is now fully up-to-date w/ system and
+    //  abstractBods tells us which BusinessObjectDef's are marked as 'abstract' 
+    //  Create a file in GridFs and start streaming
+    
+    
+    
+    //The BOP object itself will act as our 'meta' block, 
+    bop.manifest = mergedManifest;
+    bop.markModified('manifest');
+    bop.package_file = undefined;
+    
+    //Manage package-level versioning - automatically increment minor version
+    bop.major_version = bop.major_version || 0;
+    bop.minor_version = (bop.minor_version || 0) + 1;
+    
+    
+    //Stub out the package_file field value (an "attachment")
+    var attachmentMetaObj = {
+      filename: bop.key+'.'+bop.major_version+'.'+bop.minor_version+'.json',
+      type:'application/json'
+    };
+    
+    
+    //Start streaming to the output package
+    pkgStream = GridFsService.writeFile(attachmentMetaObj);
+    
+    pkgStream.on('error', function(err) {
+      deferred.reject(err);
+    });
+
+    pkgStream.write('{\n"metadata":');
+    pkgStream.write(stringify(bop));
+    
+    bop.package_file = attachmentMetaObj;
+    
+    pkgStream.write(',\n"business_objects":[\n');
+    
+    var comma = ''; //prepended to each object as written; only blank for the first one
+    
+    var streamBo = function(theObj) {
+        if(!theObj) {
+            console.error('Trying to stream BO that wasnt in the system (Should never see this)');
+            return;
+        }
+        var streamObj = theObj.toPlainObject();
+        streamObj._class = theObj._bo_meta_data.class_name;
+        delete streamObj.__v;
+        pkgStream.write(comma + stringify(streamObj));
+        comma = ',\n'; 
+    };
+    
+    var promiseChain = Q(true);  //a chain of calls, alternating between findOne and streamBo
+    var appendToChain = function(boClass, boId) {
+        promiseChain = promiseChain
+            .then(
+                db[boClass].findOne.bind(db[boClass], {_id:boId}, null, null, null) 
+            )
+            .then(
+                streamBo
+            );
+    };
+    
+    
+    //Write BusinessObjectDef's first, with abstract ones at the start
+    if(mergedManifest.BusinessObjectDef) {
+        
+        var allBodIds = Object.keys(mergedManifest.BusinessObjectDef);
+        allBodIds.sort();
+        
+        _.forEach(allBodIds, function(bodId) {
+            if(abstractBods[bodId] && mergedManifest.BusinessObjectDef[bodId] !== 'deleted') {
+                appendToChain('BusinessObjectDef', bodId);
+            }
+        });
+        _.forEach(allBodIds, function(bodId) {
+            if(!abstractBods[bodId] && mergedManifest.BusinessObjectDef[bodId] !== 'deleted') {
+                appendToChain('BusinessObjectDef', bodId);
+            }
+        });
+    }
+    
+    //Now the rest of the objects, sorted alphabetically
+    var allClasses = Object.keys(mergedManifest);
+    allClasses.sort();
+    _.forEach(allClasses, function(boClass) {
+        if(boClass !== 'BusinessObjectDef') {
+            var allIds = Object.keys(mergedManifest[boClass]);
+            allIds.sort();
+            
+            _.forEach(allIds, function(id) {
+                if(mergedManifest[boClass][id] !== 'deleted') {
+                    appendToChain(boClass, id);
+                }
+            });
+        }
+    });
+    
+    //At this point, promiseChain holds the full sequence of calls to 
+    // retrieve and stream each BO belonging to the package.
+    
+    //When that's all done...
+    promiseChain = promiseChain.then(function() {
+        pkgStream.end(']}\n'); //close off business_object array, and initial open curlybrace
+        console.log('COMPLETED PACKAGE GENERATION for %j', bop.key);
+        return bop.save();
+    });
+    
+    return promiseChain;
+    
+  }) //end Phase 2
+  .then(
+    function() {
+      console.log('generated package file %s for %s ',bop.package_file.attachment_id, bop.key);
+      deferred.resolve(bop.package_file.attachment_id);
+    },
+    function(err) {
+      deferred.reject(err);
+    }
+  );
+
+  return deferred.promise;
+  
+}; //end exports.buildPackage definition
